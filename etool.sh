62c4f84f1ad8e79eafe8d96aae8535a509603b99
jj <jj@santtu.local>
2022-03-01 14:31:41 +0200

    doc, doc

--- etool.sh
+++ etool.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 HOST_DIR=tmp/etool
-ETOOL="docker run --rm --user $(id -u):$(id -g) -e DISPLAY=unix:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/$HOST_DIR:/etool marcus2002/etool:1"
+TAG=1
+ETOOL="docker run --rm --user $(id -u):$(id -g) -e DISPLAY=unix:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/$HOST_DIR:/etool marcus2002/etool:$TAG"
 
 set -x
 $ETOOL $*
